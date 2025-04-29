f999d023da0a2a240a3c51ef0c8145ad2b44b61b
anuani-78 <anu.anunitha9@gmail.com>
2025-04-29 14:25:20 -0400

    Update clean_csv.py

--- python/clean_csv.py
+++ python/clean_csv.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+df = pd.read_csv('sample.csv')
+df_clean = df.drop_duplicates()
+df_clean.to_csv('sample_clean.csv', index=False)
+print("Duplicates removed and saved as 'sample_clean.csv'.")
